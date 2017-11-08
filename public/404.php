d1dfef1230a61e21673240989cdbb9852f161ad6
Aurelien <aurelien.mojak@gmail.com>
2017-11-08 16:59:55 +0100

    update user

---
+++ public/404.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/../inc/config.php';
+
+//A la fin, j'affiche
+require_once __DIR__.'/../view/header.php';
+require_once __DIR__.'/../view/404.php';
+require_once __DIR__.'/../view/footer.php';
---
+++ view/404.php
@@ -0,0 +1,2 @@
+<p>Chuck ne veut pas que vous accédiez à cette page</p>
+<img src="img/homer.png" alt="">
